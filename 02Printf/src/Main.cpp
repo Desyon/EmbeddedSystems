4dbcb33b8b53a8e18edfc5c8455f7315a7da1432
Til Goepfert <tilgoepfert@gmail.com>
2017-04-02 01:09:43 +0200

    added all methods except string

--- 02Printf/src/Main.cpp
+++ 02Printf/src/Main.cpp
@@ -5,6 +5,9 @@ int main() {
   char buffer[1024];
   unsigned int test = 42;
 
-  printf("%s\n", Printf(buffer, buffer + 1023, "%u %c", 42, '*'));
-  //printf("%s\n", Printf(buffer, buffer + 1023, "text %d %u %c %s %x %b %%", -42, test, '*', "test string", 0xfeed, 42));
+  printf("%s\n", Printf(buffer, buffer + 1023, "Forty Two %d %u %c %x %b %%", -42, test, '*', 42, 42));
+  //expected result: "Forty Two -42 42 * 0x2a 0b101010 %"
+
+  //printf("%s\n", Printf(buffer, buffer + 1023, "Forty Two %d %u %c %s %x %b %%", -42, test, '*', "Hello World!", 42, 42));
+  //expected result: "Forty Two -42 42 * Hello World! 0x2a 0b101010 %"
 }
--- 02Printf/src/Printf.cpp
+++ 02Printf/src/Printf.cpp
@@ -11,7 +11,15 @@
 #define BINARY 'b'          //set identifier char for binary numbers
 #define PERCENT '%'         //set identifier char for percent signs
 
-char *numberToBase(char *, const void *, unsigned int, unsigned int);
+char *printD(char *, const void *, const int);
+char *printU(char *, const void *, unsigned int);
+char *printC(char *, const void *, const char);
+char *printS(char *, const void *, char *);
+char *printX(char *, const void *, const unsigned int);
+char *printB(char *, const void *, const unsigned int);
+
+char *intToBaseString(char *, const void *, unsigned int, unsigned int);
+
 static const char digits[] = "0123456789abcdef";
 
 char *Printf(char *destination, const void *end, const char *format, ...) {
@@ -23,32 +31,42 @@ char *Printf(char *destination, const void *end, const char *format, ...) {
   va_list argList;
   va_start(argList, format);
 
-  char *nextOutput = destination;
+  char *nextChar = destination;
 
   for (const char *position = format; END_OF_STRING != *position; position++) {
     //check if formatted print is needed
     if (FORMAT_IDENT != *position) {
-      *nextOutput = *position;
-      nextOutput++;
+      nextChar = printC(nextChar, end, *position);
       continue;
     } else {
       char formatChar = *(position + 1);
       switch (formatChar) {
-        case INTEGER: break;
-        case UNSIGNED: {
-          nextOutput = numberToBase(nextOutput, end, va_arg(argList, unsigned int), 10);
+        case INTEGER:
+          nextChar = printD(nextChar, end, va_arg(argList, int));
           break;
-        }
-        case CHARACTER: {
-          *nextOutput = va_arg(argList, int);
-          nextOutput++;
+        case UNSIGNED:
+          nextChar = printU(nextChar, end, va_arg(argList, unsigned
+              int));
           break;
-        }
-        case STRING: break;
-        case HEXADECIMAL: break;
-        case BINARY: break;
-        case PERCENT: break;
-        default: return nullptr; //if unrecognized return nullptr
+        case CHARACTER:
+          nextChar = printC(nextChar, end, static_cast<char>(va_arg(argList, int)));
+          break;
+        case STRING:
+          //nextChar = printS(nextChar, end, va_arg(argList, char*));
+          break;
+        case HEXADECIMAL:
+          nextChar = printX(nextChar, end, va_arg(argList, unsigned
+              int));
+          break;
+        case BINARY:
+          nextChar = printB(nextChar, end, va_arg(argList, unsigned
+              int));
+          break;
+        case PERCENT:
+          nextChar = printC(nextChar, end, '%');
+          break;
+        default:
+          return nullptr; //if unrecognized return empty string
       }
 
       position++;   //increase position pointer to skip next char
@@ -57,25 +75,91 @@ char *Printf(char *destination, const void *end, const char *format, ...) {
 
   va_end(argList);
 
-  *nextOutput = END_OF_STRING;
+  *nextChar = END_OF_STRING;
   return destination;
 }
 
-char *numberToBase(char *output, const void *end, unsigned int value, unsigned int base) {
-  //check if an argument is empty or the output equal or behind the end of the output buffer
-  if (!output || !end || output >= end) {
-    return nullptr;
+char *printD(char *destination, const void *end, const int value) {
+  if (end <= destination) return nullptr;
+
+  if (0 > value) {
+    //print unary '-' if value is below 0
+    destination = printC(destination, end, '-');
+    //print absolute value of the number
+    destination = intToBaseString(destination, end, static_cast<unsigned int>(value * -1), 10);
+  } else {
+    destination = intToBaseString(destination, end, static_cast<unsigned int>(value), 10);
   }
+
+  return ++destination;
+}
+
+char *printU(char *destination, const void *end, const unsigned int value) {
+  if (end <= destination) return nullptr;
+
+  destination = intToBaseString(destination, end, value, 10);
+
+  return ++destination;
+}
+
+char *printC(char *destination, const void *end, const char value) {
+  if (end <= destination) return nullptr;
+
+  *destination = value;
+
+  return ++destination;
+}
+
+char *printS(char *destination, const void *end, char *value) {
+  if (end <= destination) return nullptr;
+
+  for (value; END_OF_STRING != value; value++) {
+    *destination = *value;
+    destination++;
+  }
+
+  return destination;
+}
+
+char *printX(char *destination, const void *end, const unsigned int value) {
+  if (end <= destination) return nullptr;
+
+  //print hexadecimal prefix
+  destination = printC(destination, end, '0');
+  destination = printC(destination, end, 'x');
+
+  //print hexadecimal number
+  destination = intToBaseString(destination, end, value, 16);
+
+  return ++destination;
+}
+
+char *printB(char *destination, const void *end, const unsigned int value) {
+  if (end <= destination) return nullptr;
+
+  //print binary prefix
+  destination = printC(destination, end, '0');
+  destination = printC(destination, end, 'b');
+
+  //print binary number
+  destination = intToBaseString(destination, end, value, 2);
+
+  return ++destination;
+}
+
+char *intToBaseString(char *destination, const void *end, unsigned int value, unsigned int base) {
+  if (end <= destination) return nullptr;
 
   char digit = digits[value % base];    //get char from array "digits"
   value = value / base;                 //shift the value by the amount of base
 
   //recursion if the there are still digits left
   if (value) {
-    output = numberToBase(output, end, value, base);
+    destination = intToBaseString(destination, end, value, base);
+    destination++;
   }
 
-  *output = digit;    //write the digit
-  return ++output;    //increase and return the pointer
+  *destination = digit;    //write the digit
+  return destination;    //increase and return the pointer
 }
 
